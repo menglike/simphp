e9cb0917dc122f10b7a0626c055b552f2fd7480f
menglike <liang@gutou.com>
2017-10-09 17:42:34 +0800

    增加了日志记录的静态方法

--- fw/Base/Log.class.php
+++ fw/Base/Log.class.php
@@ -1,8 +1,10 @@
 <?php
 namespace Base;
 class Log{
-	public static function test()
+	public static function record($msg)
 	{
-		
+		$logFile = PROJECT.'/runtime/cache/log/'.date('Y-m-d').'.log';
+		is_writable(PROJECT.'/runtime/cache/log') ? touch($logFile,0777) : die(PROJECT.'/runtime/cache/log 目录不可写');	 
+		file_put_contents($logFile, $msg.PHP_EOL, FILE_APPEND);
 	}
 }
